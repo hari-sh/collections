55c7ba41ad1c4b3b0cdae1cf1cd32cc28c6357de
root <root@APAC-IND-LAP021.corp.energyexemplar.com>
2020-08-15 21:26:37 +0530

    map, heap, list

--- test/map/map.c
+++ test/map/map.c
@@ -1,40 +1,91 @@
-#include<stdio.h>
-#include<stdlib.h>
 #include "map.h"
 
-int compare(void* data1, void* data2)   {
-    char* d1 = *(char**)data1;
-    char* d2 = *(char**)data2;
-    //return (d1 > d2) ? 1 : (d1 < d2) ? -1 : 0;
-    return strcmp(d1,d2);
+int comparer(void* data1, void* data2)   {
+    int d1 = *(int*)data1;
+    int d2 = *(int*)data2;
+    return (d1 > d2) ? 1 : (d1 < d2) ? -1 : 0;
+    //return strcmp(d1,d2);
 }
 
-void printd(void* data)   {
-    printf("%s ", *(char**)data);
+void printd(map_data* data)   {
+    printf("key: %d  value: %d \n", *(int*)data->key, *(int*)data->value);
 }
 
-int main(void)  {
+void main() { 
 
-    bst* bst = bst_create(sizeof(char*), compare);
-    char* data = "hari";
-	bst_push(bst, &data);
-    data = "rana";
-	bst_push(bst, &data);
-    data = "bhai";
-	bst_push(bst, &data);
-    data = "great";
-	bst_push(bst, &data);
-    data = "legend";
-	bst_push(bst, &data);
-    data = "suspect";
-	bst_push(bst, &data);
+map* map  = map_create(4,4, comparer);
+int key;
+int value;
+key = 7;
+value = 1;
 
-	bst_print_preorder(bst, printd);
-	printf("\n");
-    bst_print_inorder(bst, printd);
-	printf("\n");
-    bst_print_postorder(bst, printd);
-	printf("\n");
+map_push(map, &key, &value); 
+key = 3;
+value = 2;
+map_push(map, &key, &value); 
+key = 18;
+value = 3;
+map_push(map, &key, &value); 
+key = 10;
+value = 4;
+map_push(map, &key, &value); 
+key = 22;
+value = 5;
+map_push(map, &key, &value); 
+key = 8;
+value = 6;
+map_push(map, &key, &value); 
+key = 11;
+value = 7;
+map_push(map, &key, &value); 
+key = 26;
+value = 8;
+map_push(map, &key, &value); 
+key = 2;
+value = 9;
+map_push(map, &key, &value); 
+key = 6;
+value = 10;
+map_push(map, &key, &value); 
+key = 13;
+value = 11;
+map_push(map, &key, &value); 
 
-    return 0;
+map_traverse(map, printd);
+printf("\ncount.. %d\n", map->count);
+printf("\nDeleting 18, 11, 3, 10, 22\n"); 
+
+key = 18;
+map_pop(map, &key); 
+key = 11;
+map_pop(map, &key); 
+key = 3;
+map_pop(map, &key); 
+key = 10;
+map_pop(map, &key); 
+key = 22;
+map_pop(map, &key);
+
+map_traverse(map, printd);
+printf("\ncount.. %d\n", map->count);
+
+key = 26;
+printf("\n%d\n",*(int*)map_get(map, &key));
+
+int* keys = (int*)map_keys(map);
+
+printf("keys: \n");
+for(int i=0; i<map->count; i++) {
+    printf("%d ", keys[i]);
+}
+
+int* values = (int*)map_values(map);
+
+printf("values: \n");
+for(int i=0; i<map->count; i++) {
+    printf("%d ", values[i]);
+}
+
+map_clear(map);
+printf("%d", map->count);
 }
